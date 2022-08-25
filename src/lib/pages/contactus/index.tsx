4800c0f640371f54a7154c557d2f72e11c44b518
Maximiliano Sorribas <mjsorribas@gmail.com>
2022-08-25 10:12:37 -0300

    feat: add contactus page

---
+++ src/lib/pages/contactus/index.tsx
@@ -0,0 +1,13 @@
+import { Grid } from "@chakra-ui/react";
+
+import ContactForm from "lib/components/contactform/ContactForm";
+
+const ContactUs = () => {
+  return (
+    <Grid gap={4}>
+      <ContactForm />
+    </Grid>
+  );
+};
+
+export default ContactUs;
