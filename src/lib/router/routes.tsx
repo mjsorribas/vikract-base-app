37aa65d0b88ecf15c66b238b7776a5fb1c1ff1dc
Maximiliano Sorribas <mjsorribas@gmail.com>
2022-08-25 10:11:37 -0300

    feat: add page to routes

--- src/lib/router/routes.tsx
+++ src/lib/router/routes.tsx
@@ -1,11 +1,16 @@
 import type { PathRouteProps } from "react-router-dom";
 
 import Home from "lib/pages/home";
+import ContactUs from "lib/pages/contactus";
 
 export const routes: Array<PathRouteProps> = [
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/contactus",
+    element: <ContactUs />,
   },
 ];
 
