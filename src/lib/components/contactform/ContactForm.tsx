eab4dd9683b787a8874326fa218d5df0d7a0f558
Maximiliano Sorribas <mjsorribas@gmail.com>
2022-08-25 10:19:06 -0300

    feat: update contactus form title

--- src/lib/components/contactform/ContactForm.tsx
+++ src/lib/components/contactform/ContactForm.tsx
@@ -41,7 +41,7 @@ export default function ContactForm() {
             <Wrap spacing={{ base: 20, sm: 3, md: 5, lg: 20 }}>
               <WrapItem>
                 <Box>
-                  <Heading>Contact</Heading>
+                  <Heading>Contact Us</Heading>
                   <Text mt={{ sm: 3, md: 3, lg: 5 }} color="gray.800">
                     Fill up the form below to contact with our sales manager
                   </Text>
