ec29db628ef80cc4395a32f2750e17974f1c2be2
Maximiliano José Sorribas <mjsorribas@gmail.com>
2022-10-17 15:17:16 -0300

    feat: add aboutus page

---
+++ src/lib/pages/aboutus/index.tsx
@@ -0,0 +1,21 @@
+import { Grid, Text } from "@chakra-ui/react";
+
+const Aboutus = () => {
+  return (
+    <Grid gap={4}>
+      <Text>What is an About Us page?</Text>
+      <Text>
+        An About Us page is a spot for your founding story, a place to show off
+        your business wins, and a sales page that answers the most pressing
+        question new customers have about your business.
+        <br />
+        What an About Us page should be is a goal-oriented sales page, one that
+        focuses on highlighting the biggest selling points of your story and
+        brand at the top of the page, making a strong first impression on
+        curious customers
+      </Text>
+    </Grid>
+  );
+};
+
+export default Aboutus;
