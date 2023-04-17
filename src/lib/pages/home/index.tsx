2d8d26dfbe7f791a433efaa828897ac37043876d
Maximiliano José Sorribas <mjsorribas@gmail.com>
2023-04-17 16:59:59 -0300

    feat: update home page , add chakra provider

--- src/lib/pages/home/index.tsx
+++ src/lib/pages/home/index.tsx
@@ -1,23 +1,25 @@
-import { Grid } from "@chakra-ui/react";
+import { Grid, ChakraProvider } from "@chakra-ui/react";
 
 import { Carousel } from "lib/components/carousel/Carousel";
 
 const Home = () => {
   return (
     <Grid gap={4}>
-      <Carousel
-        data={[
-          {
-            image: "assets/carousel/code-g06a39c171_1920.jpg",
-          },
-          {
-            image: "assets/carousel/coding-gcb58870e4_1920.jpg",
-          },
-          {
-            image: "assets/carousel/software-developer-ga830c698a_1920.jpg",
-          },
-        ]}
-      />
+      <ChakraProvider>
+        <Carousel
+          data={[
+            {
+              image: "assets/carousel/code-g06a39c171_1920.jpg",
+            },
+            {
+              image: "assets/carousel/coding-gcb58870e4_1920.jpg",
+            },
+            {
+              image: "assets/carousel/software-developer-ga830c698a_1920.jpg",
+            },
+          ]}
+        />
+      </ChakraProvider>
     </Grid>
   );
 };
