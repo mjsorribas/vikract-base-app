57551014ad5a1300ea6709af665850384710f339
Maximiliano José Sorribas <mjsorribas@gmail.com>
2023-05-16 22:43:48 -0300

    feat: add demodata folder with carousel and tabs dummy info

---
+++ src/lib/demodata/tabsdata.tsx
@@ -0,0 +1,6 @@
+// eslint-disable-next-line import/prefer-default-export
+export const tabsdata = [
+  { title: "Features", content: "Content 1" },
+  { title: "Components", content: "Content 2" },
+  { title: "Documentation", content: "Content 3" },
+];
