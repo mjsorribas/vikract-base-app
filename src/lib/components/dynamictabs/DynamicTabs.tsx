3f30eee9a67f05c967e97f602229aa86f29fa43b
Maximiliano José Sorribas <mjsorribas@gmail.com>
2023-05-16 22:43:01 -0300

    feat: add dynamictabs component

---
+++ src/lib/components/dynamictabs/DynamicTabs.tsx
@@ -0,0 +1,57 @@
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import type { FC } from "react";
+
+interface TabData {
+  title: string;
+  content: string;
+}
+
+interface DynamicTabsProps {
+  data: TabData[];
+  size: string;
+  variant: string;
+  colorScheme: string;
+  bg: string;
+  align: string;
+  position: string;
+  isFitted: boolean;
+  onChange?: (index: number) => void;
+}
+
+const DynamicTabs: FC<DynamicTabsProps> = ({
+  data,
+  size,
+  variant,
+  colorScheme,
+  bg,
+  align,
+  position,
+  isFitted,
+  onChange,
+}) => {
+  return (
+    <Tabs
+      align={align}
+      variant={variant}
+      colorScheme={colorScheme}
+      bg={bg}
+      size={size}
+      isFitted={isFitted}
+      onChange={onChange}
+      position={position}
+    >
+      <TabList>
+        {data.map((item, index) => (
+          <Tab key={index}>{item.title}</Tab>
+        ))}
+      </TabList>
+      <TabPanels>
+        {data.map((item, index) => (
+          <TabPanel key={index}>{item.content}</TabPanel>
+        ))}
+      </TabPanels>
+    </Tabs>
+  );
+};
+
+export default DynamicTabs;
