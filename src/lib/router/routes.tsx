10275dd0ebda5f319cc34d67e20af4e5916a8fae
Maximiliano José Sorribas <mjsorribas@gmail.com>
2023-05-18 09:07:02 -0300

    feat: update routes, add features page

--- src/lib/router/routes.tsx
+++ src/lib/router/routes.tsx
@@ -2,6 +2,7 @@ import type { PathRouteProps } from "react-router-dom";
 
 import AboutUs from "lib/pages/aboutus";
 import ContactUs from "lib/pages/contactus";
+import Features from "lib/pages/features";
 import Home from "lib/pages/home";
 import Prices from "lib/pages/prices";
 
@@ -21,6 +22,10 @@ export const routes: Array<PathRouteProps> = [
   {
     path: "/aboutus",
     element: <AboutUs />,
+  },
+  {
+    path: "/features",
+    element: <Features />,
   },
 ];
 
