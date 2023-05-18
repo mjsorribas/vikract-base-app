e537bd1f8247ed6f6dff5e670584a55d96b1fc3c
Maximiliano José Sorribas <mjsorribas@gmail.com>
2023-05-18 09:05:49 -0300

    feat: add dynamic modal demo data

---
+++ src/lib/demodata/modaldata.tsx
@@ -0,0 +1,48 @@
+export const modalData = {
+  buttonsConfiguration: {
+    accept: {
+      title: "Accept",
+      onClick: "handleAccept",
+      bg: "blue",
+      color: "white",
+      size: "md",
+      colorScheme: "blue",
+      order: 2,
+      showButton: true,
+    },
+    cancel: {
+      title: "Cancel",
+      onClick: "handleCancel",
+      bg: "gray",
+      color: "white",
+      size: "md",
+      colorScheme: "gray",
+      order: 1,
+      showButton: true,
+    },
+    custom: {
+      title: "Custom",
+      onClick: "handleCustom",
+      bg: "green",
+      color: "white",
+      size: "md",
+      colorScheme: "green",
+      order: 3,
+      showButton: false,
+    },
+  },
+  configuration: {
+    onClose: "handleClose",
+    finalFocusRef: "finalFocusRef",
+    isOpen: "isOpen",
+    scrollBehavior: "inside",
+    size: "md",
+    isCentered: false,
+    bg: "white",
+    color: "black",
+  },
+  data: {
+    title: "Example Modal",
+    content: "This is the content of the modal.",
+  },
+};
