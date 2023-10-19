0c8bc600e86b77de9c82e38f6a782b910e2ca158
Maximiliano José Sorribas <mjsorribas@gmail.com>
2023-10-18 23:56:41 -0300

    feat(ProductCardData): update interfase

---
+++ src/lib/components/productcard/ProductCardData.tsx
@@ -0,0 +1,13 @@
+export interface ProductCardData {
+  title: string;
+  description: string;
+  price: number;
+  currency: string;
+  isAddtoCartActive: boolean;
+  isNew: boolean;
+  productImage: string;
+  productUrl: string;
+  stars: number;
+  starsReviewsCounted: number;
+  stock: number;
+}
