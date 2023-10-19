6d538c67c7d61e76a2855546d319cbd5ba399ca9
Maximiliano José Sorribas <mjsorribas@gmail.com>
2023-10-19 00:23:13 -0300

    feat(ProductsData): update example data

--- src/lib/demodata/productsdata.tsx
+++ src/lib/demodata/productsdata.tsx
@@ -2,13 +2,12 @@
 export const productsdata = [
   {
     title: "Producto 1",
-    description: "Descripción del producto 1",
+    shortDescription: "Descripción del producto 1",
     price: 19.99,
     currency: "USD",
-    isAddtoCartActive: true,
     isNew: true,
     productImage:
-      "https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=4600&q=80",
+      "https://images.unsplash.com/flagged/photo-1564826836200-87e3faad54bb?auto=format&fit=crop&q=80&w=1974&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     productUrl: "#",
     stars: 4.5,
     starsReviewsCounted: 100,
@@ -16,13 +15,12 @@ export const productsdata = [
   },
   {
     title: "Producto 2",
-    description: "Descripción del producto 2",
+    shortDescription: "Descripción del producto 2",
     price: 29.99,
     currency: "USD",
-    isAddtoCartActive: false,
-    isNew: false,
+    isNew: true,
     productImage:
-      "https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=4600&q=80",
+      "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     productUrl: "#",
     stars: 3.8,
     starsReviewsCounted: 75,
@@ -30,13 +28,12 @@ export const productsdata = [
   },
   {
     title: "Producto 3",
-    description: "Descripción del producto 3",
+    shortDescription: "Descripción del producto 3",
     price: 39.99,
     currency: "USD",
-    isAddtoCartActive: true,
-    isNew: false,
+    isNew: true,
     productImage:
-      "https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=4600&q=80",
+      "https://images.unsplash.com/photo-1484704849700-f032a568e944?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     productUrl: "#",
     stars: 4.2,
     starsReviewsCounted: 120,
@@ -44,13 +41,12 @@ export const productsdata = [
   },
   {
     title: "Producto 4",
-    description: "Descripción del producto 4",
+    shortDescription: "Descripción del producto 4",
     price: 49.99,
     currency: "USD",
-    isAddtoCartActive: true,
     isNew: true,
     productImage:
-      "https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=4600&q=80",
+      "https://images.unsplash.com/photo-1563014959-7aaa83350992?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=4600&q=80",
     productUrl: "#",
     stars: 4.9,
     starsReviewsCounted: 150,
@@ -58,13 +54,12 @@ export const productsdata = [
   },
   {
     title: "Producto 5",
-    description: "Descripción del producto 5",
+    shortDescription: "Descripción del producto 5",
     price: 59.99,
     currency: "USD",
-    isAddtoCartActive: false,
-    isNew: false,
+    isNew: true,
     productImage:
-      "https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=4600&q=80",
+      "https://images.unsplash.com/photo-1523275335684-37898b6baf30?auto=format&fit=crop&q=80&w=1999&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     productUrl: "#",
     stars: 3.5,
     starsReviewsCounted: 50,
