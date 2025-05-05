adeed1a44ce02eb49bcf804844480ebaefc296da
Maximiliano José Sorribas <mjsorribas@gmail.com>
2025-05-05 10:39:48 -0300

    feat(analytics): implement clarity on app and add microsoft package

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,11 @@ import Routings from "lib/router/Routings";
 import { theme } from "lib/styles/customTheme";
 import googleAnalyticsTool from "./lib/utils/googleAnalyticsTool";
 import { Analytics as VercelAnalytics } from "@vercel/analytics/react";
+import clarityTool from "./lib/utils/clarityAnalyticsTool";
 
 function App() {
   useEffect(() => {
+    clarityTool;
     googleAnalyticsTool.page(); // Registra la vista de página inicial
   }, []);
 
